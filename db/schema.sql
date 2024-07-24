e023517cde484a8c7f4d502e40ec2f70326d7c15
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-07-23 21:53:09 -0400

    added functions and made changes to tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,30 @@ CREATE DATABASE department_db;
 \c department_db;
 
 CREATE TABLE department (
-    department_id INTEGER NOT NULL
-    department_name VARCHAR(30) NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) UNIQUE NOT NULL,
+    SERIAL PRIMARY KEY (id)
 )
 
 CREATE TABLE roles (
-    job_title VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    salary FLOAT NOT NULL
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    SERIAL PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 )
 
-CREATE TABLE employees {
-    employee_id INTEGER NOT NULL,
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    job_title VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    salary FLOAT NOT NULL,
-    manager VARCHAR(30) NOT NULL
-}
+    role_id INTEGER NOT NULL
+    manager_id INTEGER,
+    SERIAL PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+)
--- index.js
+++ index.js
@@ -4,16 +4,212 @@ require('dotenv').config();
 
 const pool = new Pool(
     {
-        user: DB_USER,
-        password: DB_PASSWORD,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
         host: 'localhost',
-        database: DB_NAME
+        database: process.env.DB_NAME
     },
     console.log(`Connected to the database.`)
 )
 
 pool.connect()
 
-const promptUser = () => {
-    
+const initialPromptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'initialOptions',
+            message: 'What would you like to do?',
+            choices: [
+                {
+                    name: 'View all Departments',
+                    value: 'VIEW_DEPARTMENTS',
+                },
+                {
+                    name: 'View all Roles',
+                    value: 'VIEW_ROLES',
+                },
+                {
+                    name: 'View all Employees',
+                    value: 'VIEW_EMPLOYEES',
+                    
+                },
+                {
+                    name: 'Add a department',
+                    value: 'ADD_DEPARTMENT',
+                },
+                {
+                    name: 'Add a Role',
+                    value: 'ADD_ROLE',
+                },
+                {
+                    name: 'Add an Employee',
+                    value: 'ADD_EMPLOYEE',
+                },
+                {
+                    name: 'Update Employee Role',
+                    value: 'UPDATE_EMPLOYEE_ROLE',
+                },
+                {
+                    name: 'Quit',
+                    value: 'QUIT',
+                },
+            ]
+        }
+    ]).then((res) => {
+        if (res === 'VIEW_DEPARTMENTS') {
+            viewDepartments();
+        } else if (res === 'VIEW_ROLES') {
+            viewRoles();
+        } else if (res === 'VIEW_EMPLOYEES') {
+            viewEmployees();
+        } else if (res === 'ADD_DEPARTMENT') {
+            addDepartment();
+        } else if (res === 'ADD_ROLE') {
+            addRole();
+        } else if (res === 'ADD_EMPLOYEE') {
+            addEmployee();
+        } else if (res === 'UPDATE_EMPLOYEE_ROLE'){
+            updateEmployeeRole();
+        } else {
+            pool.end();
+        }
+    })
+}
+initialPromptUser();
+
+const viewDepartments = () => {
+    console.log('Viewing Departments');
+
+    const sql = `SELECT * FROM department`;
+
+    pool.query(sql, (err, {rows}) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(rows);
+    })
+}
+
+const viewRoles = () => {
+    console.log('Viewing Roles');
+
+    const sql = `SELECT * FROM roles`;
+
+    pool.query(sql, (err, {rows}) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(rows);
+    })
+}
+
+const viewEmployees = () => {
+    console.log('Viewing Employees');
+
+    const sql = `SELECT * FROM employee`;
+
+    pool.query(sql, (err, {rows}) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(rows);
+    })
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?'
+        },
+    ]).then((res) => {
+
+        const sql = `INSERT INTO department SET ?`
+
+        pool.query(sql, {
+            name: res.departmentName
+        },
+        (err, res) => {
+            if (err) {
+                console.log(err)
+            }
+            console.table(res);
+    })
+    })
+}
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary for this role?'
+        },
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'What is the name of the department?'
+        },
+    ]).then((res) => {
+        const sql = `INSERT INTO roles SET ?`;
+
+        pool.query(sql, {
+            title: res.roleName,
+            salary: res.roleSalary,
+            // department_id
+        },
+        (err, res) => {
+            if (err) {
+                console.log(err);
+            }
+            console.table(res);
+    });
+    });
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: `What is the employee's first name?`,
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the employee's last name?",
+        },
+        {
+            type: 'input',
+            name: 'employeeRole',
+            message: "What is the employee's role?",
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: "Who is the employee's manager?",
+        },
+    ]).then((res) => {
+        const sql = `INSERT INTO employee SET ?`
+
+        pool.query(sql, {
+            first_name: res.first_name,
+            last_name: res.last_name,
+            //role_id
+            //manager_id
+        }, 
+        (err, res) => {
+            if (err) {
+                console.log(err)
+            }
+            console.table(res);
+    });
+    });
 }
